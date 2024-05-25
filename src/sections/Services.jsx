d3296eeda09aefba44a4fe8f60a5ac3d0a5383b3
Siddhant Prasad <siddhantprasad.tvnl@gmail.com>
2024-05-25 23:06:12 +0530

    Services and Special Offer Section

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import ServiceCard from '../components/ServiceCard';
+import {services} from '../constants'
+
 
 const Services = () => {
   return (
-    <div>
-    Hare Ram Hare Ram
-    </div>
+    <section className='max-container flex justify-center flex-wrap gap-9'>
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
   )
 }
 
